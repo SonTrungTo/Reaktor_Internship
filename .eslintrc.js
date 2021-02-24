b7230d3d1b8d9b1e5d5243a53c5f6edc1da8e703
SonTrungTo <son.trung.to@gmail.com>
2021-02-24 19:37:33 +0200

    added eslint, reinstalling @typescript-eslint/eslint-plugin
    - Bug: @typescript-eslint/eslint-plugin
    does not install correctly.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
+module.exports =
 {
-    "plugins": [
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: [
         "react",
         "@typescript-eslint"
     ],
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:@typecript-eslint/recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
     ],
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        es6: true,
+        browser: true
     },
-    "rules": {
+    rules: {
         "@typescript-eslint/semi": ["error"],
         "@typescript-eslint/explicit-function-return-type": 0,
         "@typescript-eslint/no-unused-vars": [
@@ -24,5 +33,11 @@
         "react/prop-types": 0,
         "@typescript-eslint/no-unsafe-member-access": 0,
         "@typescript-eslint/explicit-module-boundary-type": 0
+    },
+    settings: {
+        react: {
+            pragma: "React",
+            version: "detect"
+        }
     }
 }
